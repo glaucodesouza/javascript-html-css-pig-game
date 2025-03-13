b3128b0ec4ca9944164b8b94790ff505689749ba
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2025-03-13 16:57:26 -0300

    Starting conditions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict';
 
 //Selecting Elements
-const score0El = Document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score0El = document.querySelector('#score--0');//et the score element
+const score1El = document.getElementById('score--1'); //get the score element
+const diceEl = document.querySelector('.dice');//get the dice class
 
+//Starting conditions
 score0El.textContent = '0';
 score1El.textContent = '0';
+diceEl.classList.add('hidden'); //hide the dice now, using hidden class
 
